4f09f8d8a4e1cd4dd4cbe3e4044e9d4cc632eb60
angyaguilar <aaguilar@elon.edu>
2023-05-24 01:28:58 -0400

    delt

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, SwaptUser, Swapt_admin, Code, propManager
-
-# Registering the models in this app so the admin can view and edit these types of objects
-admin.site.register(User)
-admin.site.register(SwaptUser)
-admin.site.register(Swapt_admin)
-admin.site.register(propManager)
-admin.site.register(Code)
